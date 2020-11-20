e7a730f46397da9fe13108b8bddeb867ca1632ce
Befa222 <72620186+Befa222@users.noreply.github.com>
2020-11-20 16:30:04 +0000

    updated title

--- src/component/home.js
+++ src/component/home.js
@@ -3,21 +3,18 @@ import "./home.css"
 
 function Home () {
     return (
-      <div  className="home" >
+      <div className="home">
         <div>
-          <img className="hero-img" src="https://i.blogs.es/17b4fe/pokemon-shuffle/1366_2000.jpg"/>
+            <h1 className="title">PICK-a-CHEW</h1>
         </div>
         <div className="container-home">
-          <img className="pikachu-home" src="https://cdnb.artstation.com/p/assets/images/images/023/566/647/original/andy-pelaez-pikachu-animated.gif?1579627222"/>
+          <img className="pikachu-home" alt="pikachu" src="https://cdnb.artstation.com/p/assets/images/images/023/566/647/original/andy-pelaez-pikachu-animated.gif?1579627222"/>
         
-          <img className="poke-bubble" src="https://www.pngkit.com/png/full/584-5843757_pokemon-dialog-box-pokemon-text-box-png.png"/>
+          <img className="poke-bubble" alt="text-area" src="https://www.pngkit.com/png/full/584-5843757_pokemon-dialog-box-pokemon-text-box-png.png"/>
           
           <button className="home-button">I am hungry</button>
-          </div>
-        
-        
+        </div>
       </div>
-
     )
 }
 
