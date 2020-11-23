b2d886406cc2cbd0f0d43dc14bba4928e000605f
Befa222 <72620186+Befa222@users.noreply.github.com>
2020-11-23 17:16:18 +0000

    css ingredients test

--- src/component/ingredients.js
+++ src/component/ingredients.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PokeBubbleText from './PokebubbleText'
+import "./ingredients.css"
 
 
 
@@ -12,8 +14,28 @@ class Ingredients extends Component {
         return (
             this.state.showIngredients &&
             <div className="ingredients">
-                <h1>I'am the ingredients page</h1>
-                <button className="home-button" onClick={this.props.toResults}>I'am hungry</button>
+                <div className="ingredients-header">
+                    <img className="pikachu-ingredients" src="https://www.vhv.rs/dpng/d/46-461746_pokemon-clipart-hd-png-download.png" alt="pikachu"/>
+                        <div className="bubble-ingredients">
+                        <PokeBubbleText 
+                        textData={["Pika-pi! It's time to check what food you have at home", "Select the ingredients in the lists below!"]}
+                        />
+                    </div>
+                </div>
+
+                <section className="ingredients-section">
+                    <p>MEAT</p>
+                    <img className="ingredients-image" src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages.emojiterra.com%2Fgoogle%2Fandroid-pie%2F512px%2F1f356.png" alt="meat"/>
+                  <input type="text"></input>
+                </section>
+
+                <section className="ingredients-section">
+                    <p>VEGETABLE</p>
+                    <img className="ingredients-image" src="https://cdn2.iconfinder.com/data/icons/veggies-2/500/vegetable-plant-food-ingredients-22-512.png" alt="carot"/>
+                    
+                </section>
+
+                
             </div>
         )
     }
