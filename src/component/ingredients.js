7e1847247f88b20303a7f8bc0b53e59d0cae4ff0
Alfred <castellum.a@gmail.com>
2020-11-23 22:54:07 +0100

    UPDATES: - Commented poke-bubble class in css & added a background color to typerDiv. - Fixed the background image in the homepage to cover the entire screen. - Reduced the width of the button to 80% from 100% and centered it in both pages. - Reduced the width of the search bars in Ingredients page and centered them, also added border-radius. - Added search Icon to both search bars in Ingredients page.

--- src/component/ingredients.js
+++ src/component/ingredients.js
@@ -3,7 +3,6 @@ import PokeBubbleText from './PokebubbleText'
 import "./ingredients.css"
 
 
-
 class Ingredients extends Component {
 
     state = {
@@ -17,25 +16,27 @@ class Ingredients extends Component {
                 <div className="ingredients-header">
                     <img className="pikachu-ingredients" src="https://www.vhv.rs/dpng/d/46-461746_pokemon-clipart-hd-png-download.png" alt="pikachu"/>
                         <div className="bubble-ingredients">
-                        <PokeBubbleText 
-                        textData={["Pika-pi! It's time to check what food you have at home", "Select the ingredients in the lists below!"]}
-                        />
-                    </div>
+                            <PokeBubbleText 
+                            textData={["Pika-pi! It's time to check what food you have at home", "Select the ingredients in the lists below!"]}
+                            />
+                        </div>
                 </div>
 
                 <section className="ingredients-section">
                     <p>MEAT</p>
                     <img className="ingredients-image" src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages.emojiterra.com%2Fgoogle%2Fandroid-pie%2F512px%2F1f356.png" alt="meat"/>
-                    <input type="text"></input>
+                    <input className="searchBar" type="text"></input>
                 </section>
 
                 <section className="ingredients-section">
                     <p>VEGETABLE</p>
                     <img className="ingredients-image" src="https://cdn2.iconfinder.com/data/icons/veggies-2/500/vegetable-plant-food-ingredients-22-512.png" alt="carot"/>
-                    <input type="text"></input>
+                    <input className="searchBar" type="text"></input>
                 </section>
 
+                <div className="container-home">
                 <button className="home-button" onClick={this.props.toIngredients}>What can I cook ?</button>
+                </div>
             </div>
         )
     }
